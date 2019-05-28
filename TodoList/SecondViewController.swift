6969e1d83ef7d91159ede634686dd20758f458b0
Benedito Alberto Macedo <benedito.macedo@gmail.com>
2019-05-27 23:03:44 -0300

    Initial Commit

---
+++ TodoList/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TodoList
+//
+//  Created by Benedito Macedo on 27/05/19.
+//  Copyright © 2019 bamacedo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
